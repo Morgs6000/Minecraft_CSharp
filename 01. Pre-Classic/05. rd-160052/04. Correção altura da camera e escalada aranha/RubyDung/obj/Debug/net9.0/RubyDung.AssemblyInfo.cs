0f0cd4173e4b740381004d639e0ca5e2b0fec0bf
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2025-12-30 12:01:32 -0300

    Commit Update

--- 01. Pre-Classic/05. rd-160052/04. Correção altura da camera e escalada aranha/RubyDung/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
+++ 01. Pre-Classic/05. rd-160052/04. Correção altura da camera e escalada aranha/RubyDung/obj/Debug/net9.0/RubyDung.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e746915479edc6618556a2b6211e1c167203c18f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b704832e6e13c2c81c6193a3cf716f6f8aba27")]
 [assembly: System.Reflection.AssemblyProductAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
